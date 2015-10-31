e28c055038c22956fbef7855a650d674c78f7c12
daniloqb <daniloqb@gmail.com>
2015-10-30 22:31:44 -0200

    socket + json test

---
+++ tcpclient.py
@@ -0,0 +1,21 @@
+import socket
+import simplejson as json
+
+__author__ = 'tic'
+
+host = 'localhost'
+port = 50001
+size = 1024
+
+d_json = {'led':'on','rgb':['255','100','50']}
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((host, port))
+s.send(json.dumps(d_json))
+data = s.recv(1024)
+s.close()
+
+j = json.loads(data)
+
+print json.dumps(j,sort_keys=True, indent=4 * '')
+
