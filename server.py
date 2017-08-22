b04efd7fa6878a05e0086b943af791eddd1c4efa
daniloqb <daniloqb@gmail.com>
2017-08-22 12:49:10 -0300

    changes

---
+++ server.py
@@ -0,0 +1,28 @@
+import socket
+import simplejson as json
+
+__author__ = 'tic'
+
+
+
+host = ''
+port = 2121
+size = 1024
+backlog = 5
+
+
+
+
+
+s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+s.bind((host,port))
+s.listen(backlog)
+
+while 1:
+    client,address = s.accept()
+    data = client.recv(size)
+    if data:
+        print "Server received:", data
+
+        #client.send(data)
+    client.close()
